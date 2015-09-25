b70194ec0be91920885a290d4efdd96921bf3d64
backslash112 <yangcun@live.com>
2015-09-25 11:00:35 +0800

    add two views to the SearchPage view

--- SearchPage.js
+++ SearchPage.js
@@ -23,7 +23,47 @@ var styles = StyleSheet.create({
 		padding: 30,
 		marginTop: 65,
 		alignItems: 'center'
-	}
+	},
+	// searchInput
+	searchInput: {
+		height: 36,
+		padding: 4,
+		marginRight: 5,
+		flex: 4,
+		fontSize: 18,
+		borderWidth: 1,
+		borderColor: '#48bbec',
+		borderRadius: 8,
+		color: '#48bbec'
+	},
+
+	// button
+	button: {
+		height: 36,
+		flex: 1,
+		flexDirection: 'row',
+		backgroundColor: '#48bbec',
+		borderColor: '#48bbec',
+		borderWidth: 1,
+		borderRadius: 8,
+		marginBottom: 10,
+		alignSelf: 'stretch',
+		justifyContent: 'center'		
+	},
+
+	// buttonText
+	buttonText: {
+		fontSize: 18,
+		color: 'white',
+		alignSelf: 'center'
+	},
+	
+	// flowRight
+	flowRight: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		alignSelf: 'stretch'
+	},
 });
 
 class SearchPage extends Component {
@@ -36,6 +76,19 @@ class SearchPage extends Component {
 				<Text style={styles.description}>
 					Serach by place-name, postcode or search near your location.
 				</Text>
+
+				<View style={styles.flowRight}>
+					<TextInput style={styles.searchInput}
+					placeholder='Search via name or postcode' />
+					<TouchableHighlight style={styles.button} 
+					underlayColor='#99d9f4'>
+						<Text style={styles.buttonText}>Go</Text>
+					</TouchableHighlight>
+				</View>
+				<TouchableHighlight style={styles.button} 
+				underlayColor='#99d9f4'>
+					<Text style={styles.buttonText}>Location</Text>
+				</TouchableHighlight>
 			</View>
 			);
 	}
