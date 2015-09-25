f26d5612641af416b9f57f086f727f61005288b9
backslash112 <yangcun@live.com>
2015-09-25 14:30:42 +0800

    show the results

--- SearchResults.js
+++ SearchResults.js
@@ -11,10 +11,39 @@ var {
 	Component
 } = React;
 
+var styles = StyleSheet.create({
+	thumb: {
+		width: 80,
+		height: 80,
+		marginRight: 10
+	},
+	textContainer: {
+		flex: 1
+	},
+	separator: {
+		height: 1,
+		backgroundColor: '#dddddd',
+	},
+	price: {
+		fontSize: 25,
+		fontWeight: 'bold',
+		color: '#48bbec'
+	},
+	title: {
+		fontSize: 20,
+		color: '#656565'
+	},
+	rowContainer: {
+		flexDirection: 'row',
+		padding: 10
+	}
+});
+
 class SearchResults extends Component {
 	
 	constructor(props) {
 		super(props);
+
 		var dataSource = new ListView.DataSource(
 			{ rowHasChanged: (r1, r2) => r1.guid !== r2.guid });
 		this.state = {
@@ -23,11 +52,21 @@ class SearchResults extends Component {
 	}
 
 	renderRow(rowData, sectionID, rowID) {
+		var price = rowData.price_formatted.split(' ')[0];
+
 		return (
 			<TouchableHighlight
-				underlayColor = '#dddddd'>
-				<View>
-					<Text>{rowData.title}</Text>
+				onPress={() => this.rowPressed(rowData.guid)}
+				underlayColor='#dddddd'>
+				<View >
+					<View style={styles.rowContainer}>
+						<Image source={{uri: rowData.img_url}} style={styles.thumb} />
+						<View style={styles.textContainer}>
+							<Text style={styles.price}>£{price}</Text>
+							<Text style={styles.title} numberOfLines={1}>{rowData.title}</Text>
+						</View>
+					</View>
+					<View style={styles.separator} />
 				</View>
 			</TouchableHighlight>
 			);
@@ -38,6 +77,10 @@ class SearchResults extends Component {
 			<ListView dataSource = {this.state.dataSource}
 			renderRow={this.renderRow.bind(this)} />
 			);
+	}
+
+	rowPressed(propertyGuid) {
+		var property = this.props.listings.filter(prop => prop.guid === propertyGuid)[0];
 	}
 }
 
